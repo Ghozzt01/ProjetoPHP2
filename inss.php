92ee4ab61466e12a844019d008e637a665fbc8c1
SRxMNz <edna.32limalima@gmail.com>
2025-09-02 22:06:49 -0300

    slakkk

--- inss.php
+++ inss.php
@@ -1,65 +1,34 @@
 <?php 
+echo "====================================CALCULO DE IMPOSTO DE RENDA E INSS==================================== <br> <br>";
 
-echo("Sistema de folha de pagamento <br/><br/> ");
-echo("salario folha: R$ <br/>");
-echo("Desconto INSS: R$ <br/>");
-echo("Desconto IR: R$ <br/>");
 
-$salario=1690;
-$inss = 0;
-$ir = 0
+// Variaveis
+$salario = 1000;
+$inss = 14;
+$ir = 0;
+$resultado = 0;
 
-function inss($salario){
-	if($salario <= 1518){
-		$resultado = $salario * 7.5 / 100;
-		$inss = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado");
-	}
-	elseif ($salario > 1518 || $salario <= 2793.88) {
-		$resultado = $salario * 9 / 100;
-		$inss = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado"); 
-		
-	}
-	elseif ($salario > 2793.89 || $salario <= 4190.83) {
-		$resultado = $salario * 12 / 100;
-		$inss = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado"); 
-		
-	}
-	elseif ($salario > 4190.84 || $salario <= 8157.81) {
-		$resultado = $salario * 14 / 100;
-		$inss = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado"); 
-		
-	}
+if ($salario > 2259.21 || $salario <= 2826.65){
+	$ir = 7.5;
+}
+elseif ($salario > 2826.66 || $salario <= 3751.05){
+	$ir = 15;	 
+}
+elseif ($salario > 3751.06 || $salario <= 4664.68){
+	$ir = 22.5;
+}		
+elseif ($salario > 4664.68) {
+	$ir = 27.5;	
 }
 
-inss($salario);
 
-function ir($salario){
-	if($salario <= 2259.20){
-		echo("o desconto aplicado no seu salario será de: 0 R$");
-	}
-	elseif ($salario > 2259.21 || $salario <= 2826.65) {
-		$resultado = $salario * 7.5 / 100;
-		$ir = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado"); 
-		
-	}
-	elseif ($salario > 2826.66 || $salario <= 3751.05) {
-		$resultado = $salario * 15 / 100;
-		$ir = $resultado;
-		echo("o desconto aplicado no seu salario será de : $resultado"); 
-		
-	}
-
-}
-
-ir($salario);
-
-
-
+$resultado = $salario - ($salario * $inss / 100);
+$liquido = $resultado - ($resultado * $ir / 100);
+echo("Salario Folha: $salario <br>");
+echo("Desconto INSS: $inss% <br>");
+echo("Valor com o desconto do inss: $resultado <br>");
+echo("Desconto I.R.: $ir <br>");
+echo("Salario liquido: $liquido<br>");
 
 
 
