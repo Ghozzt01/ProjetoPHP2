f3f812d727084565de2f4b5f54eeea32f52d5d29
Emanuel Victor <edna.32limalima@gmail.com>
2025-09-09 20:15:23 -0300

    Add files via upload

--- repeticao.php
+++ repeticao.php
@@ -1,4 +1,21 @@
 <?php 
-	echo("Trabalhando com estrutura de repetição <br/>");
+	$soma1=10;
+	$soma2=10;
+	$resp= $soma1 + $soma2;
 
+
+	if($resp > 20){
+		$resp = $resp + 8;
+		echo("a resposta da soma: $soma1 + $soma2 + 8 é igual a $resp" );
+	}
+
+	else if($resp <= 20){
+		$resp = $resp - 5;
+		echo("a resposta da soma: $soma1 + $soma2 - 5 é igual a $resp" );
+	}
+	
+
+
+
+	
  ?>
