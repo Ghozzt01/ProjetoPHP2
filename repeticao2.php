8af9799e75fbd42790c2d246a18ffec0ca2eeb60
Emanuel Victor <edna.32limalima@gmail.com>
2025-09-09 21:41:29 -0300

    sla kk

---
+++ repeticao2.php
@@ -0,0 +1,28 @@
+<?php 
+
+	$num1 = 10;
+	$num2 = 5;
+
+	if ($num2 == 2 || $num2 == 5 || $num2 == 10) {
+		$num1 % $num2;
+
+
+
+		if($num1 % $num2 == 0){
+			echo ("o numero: $num1 é divisivel por $num2");
+		}
+		else{
+			echo "o numero: $num1 não é divisivel por: $num2";
+		}
+
+		
+	
+	}
+	else{
+			echo "o numero: $num1 não é divisivel por: $num2";
+		}
+
+ ?>
+
+
+
