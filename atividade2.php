6914dddc75c108d310a18005552227409ef8b08e
Emanuel Victor <edna.32limalima@gmail.com>
2025-09-16 20:21:17 -0300

    f de farinha

---
+++ atividade2.php
@@ -0,0 +1,30 @@
+<?php 	
+
+	/*$c = 1;
+
+	while ($c <= 100) {
+		
+		if ($c % 10 == 0) {
+			echo "$c é multiplo de 10! <br>";
+		}else{
+			echo "$c <br>";
+		}
+
+		$c = $c + 1;
+	};*/
+
+	$f = 1;
+	$i = 1;
+
+	while ($i < 65) {
+
+		$f = $f * 2;
+		$i ++;
+		
+	};
+
+	echo "$f é o valor do pagamento";
+
+
+
+ ?>
